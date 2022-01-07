99beadecfb34ffbbf93be0d29c6ba48f676cc3c9
Ekko306 <1186179435@qq.com>
2022-01-07 11:17:37 +0800

    syp-增加封装request 方便请求数据

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,8 +1,9 @@
 import './index.css';
-import { Button,Input,Space, message } from 'antd';
-import { UserOutlined,EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
-import { useNavigate } from "react-router";
+import {Button, Input, Space, message} from 'antd';
+import {UserOutlined, EyeInvisibleOutlined, EyeTwoTone} from '@ant-design/icons';
+import {useNavigate} from "react-router";
 import {useState} from "react";
+import {get, post} from "../../services/request";
 
 
 function Login() {
@@ -15,27 +16,28 @@ function Login() {
             <div className='login'>
                 <Space direction="vertical">
                     <Input
-                        onChange={(e)=>{
+                        onChange={(e) => {
                             console.log(e.target.value, '账号')
-                            setName(e.target.value)}
+                            setName(e.target.value)
+                        }
                         }
                         placeholder="default size"
-                        prefix={<UserOutlined />} />
-                    <br />
+                        prefix={<UserOutlined/>}/>
+                    <br/>
                     <Input.Password
                         onChange={(e) => {
                             console.log(e.target.value, '密码')
                             setPassword(e.target.value)
                         }}
                         placeholder="input password"
-                        iconRender={visible => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+                        iconRender={visible => (visible ? <EyeTwoTone/> : <EyeInvisibleOutlined/>)}
                     />
                     <br/>
                     <Button
                         type="primary"
                         block
-                        onClick={()=>{
-                            if(name === 'minyue' && password === '123') {
+                        onClick={() => {
+                            if (name === 'minyue' && password === '123') {
                                 message.success('登录成功')
                                 navigate('/back')
                             } else {
@@ -46,6 +48,29 @@ function Login() {
                 </Space>
                 <br/>
             </div>
+            <button
+                onClick={async () => {
+                    let param = {
+                        location: '保定',
+                        key: '4e06d85115bc4e44ad8f9f2246c82eef'
+                    }
+                    let result = await get('/v2/city/lookup', param)
+                    console.log(result, '结果1')
+                }}>
+                测试async/await请求
+            </button>
+            <button
+                onClick={() => {
+                    let param = {
+                        location: '武汉',
+                        key: '4e06d85115bc4e44ad8f9f2246c82eef'
+                    }
+                    get('/v2/city/lookup', param).then((res) => {
+                        console.log(res, '结果2')
+                    })
+                }}>
+                测试回调请求
+            </button>
         </div>
     );
 }
